b5519dd76bea3c26f632eaabf58cc1ec3abf3c48
lora_y <lora@yelcick.org>
2025-09-08 19:05:58 -0400

    Added if statement

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,11 @@
 user_input = input("Which birthday are you looking for? ")
+
+
+if user_input == "Mark":
+    print ("Birthday is 05/05/1995!")
+elif user_input == "Melissa":
+    print ("Birthday is 11/02/2002!")
+elif user_input == "Emily":
+    print ("Birthday is 02/05/2005!")
+else:
+    print ("Can't find a Birthday for " + user_input + ".")
