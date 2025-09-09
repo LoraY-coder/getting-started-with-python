e00de3d9022c445d1256c52eb2dc6b8495c73388
lora_y <lora@yelcick.org>
2025-09-09 12:48:05 -0400

    Added datetime

--- start-your-drawing-app/main.py
+++ start-your-drawing-app/main.py
@@ -1,24 +1,13 @@
 import turtle as turtle
-turtle.pencolor("purple")
-turtle.forward(100)
-turtle.right(60)
+def draw_side(pen_color, length, rotation):
+    turtle.pencolor(pen_color)
+    turtle.forward(length)
+    turtle.right(rotation)
 
-turtle.pencolor("green")
-turtle.forward(100)
-turtle.right(60)
+draw_side ("purple",110,216)
+draw_side ("purple",110,216)
+draw_side ("purple",110,216)
+draw_side ("purple",110,216)
+draw_side ("purple",110,216)
 
-turtle.pencolor("purple")
-turtle.forward(100)
-turtle.right(60)
 
-turtle.pencolor("green")
-turtle.forward(100)
-turtle.right(60)
-
-turtle.pencolor("purple")
-turtle.forward(100)
-turtle.right(60)
-
-turtle.pencolor("green")
-turtle.forward(100)
-turtle.right(60)
