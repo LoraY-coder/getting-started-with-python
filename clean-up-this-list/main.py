d8c546c4a87ef21a5785f347d402c7aea9d41ad2
lora_y <lora@yelcick.org>
2025-09-09 14:15:54 -0400

    Cleaned up the list

--- clean-up-this-list/main.py
+++ clean-up-this-list/main.py
@@ -1,1 +1,10 @@
 websites = ["www.google.com","www.youtube.com","www.dropbox.com","www.quora.com","www.reddit.com","www.instagram.com"]
+
+websites.pop()
+
+websites.insert(2,"www.yahoo.com" )
+
+websites.append("www.skillcrush.com")
+
+print ("websites = ")
+print (websites)
