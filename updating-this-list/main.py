2e5c7cfa3ca26cee61a28339249623e8e903a760
lora_y <lora@yelcick.org>
2025-09-09 14:39:22 -0400

    Updated a list of names

---
+++ updating-this-list/main.py
@@ -0,0 +1,12 @@
+names=["John", "Jim","Betty","Kathy","Karen","Bob","Kim", "Tim","Tom","Beth","Jo","Lu","Lin","Lynn","Max","Ned","Terry", "Kali","Sandy", "Stu"]
+small_group = names[:4]
+print(small_group)
+
+drink_group = names[16:]
+print(drink_group)
+
+hike_group = names[10:14]
+print(hike_group)
+
+walk_group = names[8]
+print(walk_group)
