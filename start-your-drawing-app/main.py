422a8f08ddc66dd8d4d645576e41f9b5a12ebd89
lora_y <lora@yelcick.org>
2025-09-09 11:51:15 -0400

    Added turtle module to draw hexagon

---
+++ start-your-drawing-app/main.py
@@ -0,0 +1,24 @@
+import turtle as turtle
+turtle.pencolor("purple")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("green")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("purple")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("green")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("purple")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("green")
+turtle.forward(100)
+turtle.right(60)
