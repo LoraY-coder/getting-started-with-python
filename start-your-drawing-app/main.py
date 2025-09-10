c5f9e6c00c8d30fd0c6b7d5564fd6202c303f514
lora_y <lora@yelcick.org>
2025-09-10 17:08:31 -0400

    Added input from user to choose shape

--- start-your-drawing-app/main.py
+++ start-your-drawing-app/main.py
@@ -1,13 +1,69 @@
 import turtle as turtle
+
+done=False
+
+print("Welcome to your drawing app! Select a shape you want to draw. After typing the number and hitting enter, the drawing app will draw the shape you selected.")
+
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(300)
+
+turtle.pendown()
+
+turtle.write("Drawing App!", font=("Arial", 16, "normal"))
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(50)
+turtle.pendown()
+
+
+turtle.write("To draw a shape, go to the Console tab and choose an option!", font=("Arial", 16, "normal"))
+
+turtle.penup()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.pendown()
+
+
 def draw_side(pen_color, length, rotation):
     turtle.pencolor(pen_color)
     turtle.forward(length)
     turtle.right(rotation)
 
-draw_side ("purple",110,216)
-draw_side ("purple",110,216)
-draw_side ("purple",110,216)
-draw_side ("purple",110,216)
-draw_side ("purple",110,216)
+
+def star(color):
+    for i in range (0,5):
+        draw_side (color,110,216)
+
+def hexagon(color):
+    for i in range (0,6):
+        draw_side (color,100, 60)
+
+def square(color):
+    for i in range (0,4):
+        draw_side (color,100, 90)
+
+while not done:
+    shape = input("Choose a shape: 1 for star, 2 for hexagon, 3 for square, 4 for done:  ")
+   
+    if shape == "4":
+        done=True
+    else:
+        color = input("Enter a color:  ")
+        if shape == "1":
+            star(color)
+        elif shape =="2":
+            hexagon(color)
+        else:
+            square(color)
+
+
+
+
 
 
