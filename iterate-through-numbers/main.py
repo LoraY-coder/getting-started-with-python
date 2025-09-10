c023d639c119acb53b4c37ccedeb374dc6f47b06
lora_y <lora@yelcick.org>
2025-09-10 15:35:24 -0400

    Added for loop

--- iterate-through-numbers/main.py
+++ iterate-through-numbers/main.py
@@ -1,2 +1,6 @@
 years_list = [5,7,12,13,9,21,11,10]
 num_eligible = 0
+for years in years_list:
+    if years > 9:
+        num_eligible+=1
+print (f"{num_eligible} have 10 or more years on the job.")
