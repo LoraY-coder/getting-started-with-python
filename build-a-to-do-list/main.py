1958b6881b109df1a431ad79be08909de681f625
lora_y <lora@yelcick.org>
2025-09-10 15:50:23 -0400

    Added while loop

--- build-a-to-do-list/main.py
+++ build-a-to-do-list/main.py
@@ -3,3 +3,10 @@ done = False
 
 print("Type 'done' at any time to exit")
 
+while not done: 
+    new_item = input ("Add an item to the to-do list.   ")
+    if new_item =="done":
+        done = True
+    else:
+        todo_list.append(new_item)
+print(f"The final to-do list is: {todo_list}")
