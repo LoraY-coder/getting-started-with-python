f3fca51bf9c55c1327cf62fce8ca3196cbf6c09f
lora_y <lora@yelcick.org>
2025-09-11 13:40:31 -0400

    Added while loop to translate

--- build-a-language-translator/main.py
+++ build-a-language-translator/main.py
@@ -7,3 +7,13 @@ translations = {
 done = False
 
 print('Type "done" at any time to exit')
+
+while not done:
+    word = input("Input a word in English to get the Spanish word:  ")
+    word = word.lower()
+    if word == "done":
+        done = True
+    elif translations.get(word):
+        print(translations.get(word) + " is Spanish for " + word)
+    else:
+        print(word + " is not in the dictionary.")
