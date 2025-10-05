6adaf7be52a21ab88b521ef9db387f9b007d4022
lora_y <lora@yelcick.org>
2025-10-05 11:12:34 -0400

    Added functions to assisstant app

--- start-your-personal-assitant-class/main.py
+++ start-your-personal-assitant-class/main.py
@@ -6,6 +6,32 @@ class PersonalAssistant:
 
     self.todos = []
 
+  def add_todo(self, new_item):
+    self.todos.append(new_item)
+
+  def remove_todo(self, todo_item):
+    if todo_item in self.todos:
+      # Get the todo_item index in list
+      index = self.todos.index(todo_item)
+      # pop the index for todo_item in todos list
+      self.todos.pop(index)
+    else:
+      print("Todo is not in list!")
+
+  def get_todos(self):
+    return self.todos
+  
+  def get_birthday(self, name):
+    if name == "Mark":
+      print (name + "'s Birthday is 05/05/1995!")
+    elif name == "Melissa":
+      print (name + "'s Birthday is 11/02/2002!")
+    elif name == "Emily":
+      print (name + "'s Birthday is 02/05/2005!")
+    else:
+      print ("Can't find a Birthday for " + name + ".")
+
+
   # Complete the get_contact function code
   def get_contact(self, name):
     if name in self.contacts:
@@ -17,4 +43,9 @@ class PersonalAssistant:
 # Code to test output of the class
 assistant = PersonalAssistant()
 # Change name to one from your contacts
-print(assistant.get_contact("Sam"))
+print(assistant.get_contact("Mark"))
+
+assistant.add_todo("Pick up groceries")
+print(assistant.get_todos())
+
+print(assistant.get_birthday("Emily"))
